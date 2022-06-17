0f30462a27fc3494a82b02c9411947a502c4551d
Christophe Tav <christophe.tav@gmail.com>
2022-06-17 19:14:39 -0400

    update

---
+++ scripts/spaceranger_aggr/spaceranger_aggr_ABCD_mapped.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=128G
+#SBATCH --account=def-stbil30
+#SBATCH --mail-user=christophe.tav@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=spaceranger_aggr_ABCD_mapped
+#SBATCH --output=/home/chris11/projects/def-stbil30/chris11/SpatialTranscriptomics_project/output/log/spaceranger_aggr_ABCD_mapped_log.txt
+
+spaceranger aggr --id=aggr_ABCD_mapped \
+                 --csv=input/aggrcsv_ABCD.csv \
+                 --normalize=mapped
